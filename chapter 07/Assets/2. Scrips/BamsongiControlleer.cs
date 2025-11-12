a28193e0092993845ce71aaab27242913208e41c
shingh1001 <a2005s1001@gmail.com>
2025-11-12 17:47:40 +0900

    chap 07 inging
    d

---
+++ chapter 07/Assets/2. Scrips/BamsongiControlleer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiController : MonoBehaviour
+{
+    public int forwardForece = 2000;
+    public int upForce = 800;
+
+    // Start is called before the first frame update
+
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        Shoot(new Vector3(0, upForce, forwardForece));
+       // Shoot(new Vector3(0, 200, 200));
+    }
+}
